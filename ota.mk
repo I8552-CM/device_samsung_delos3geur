b13c6c2c3c7bcd0365801e3296c929126a06f34d
Sumit Rajdev <androidlover5842@gmail.com>
2016-12-27 12:15:41 +0530

    Update ota.mk

--- ota.mk
+++ ota.mk
@@ -1,5 +1,5 @@
-PRODUCT_PROPERTY_OVERRIDES += ro.updater.uri=https://raw.githubusercontent.com/I8552-CM/Ota/delos-cm13/Updater.xml
-PRODUCT_PROPERTY_OVERRIDES += ro.updater.oldrelease.url=https://raw.githubusercontent.com/I8552-CM/Ota/delos-cm13/updater-old-release.json 
+PRODUCT_PROPERTY_OVERRIDES += ro.updater.uri=https://raw.githubusercontent.com/I8552-CM/Ota/delos-pac/Updater.xml
+PRODUCT_PROPERTY_OVERRIDES += ro.updater.oldrelease.url=https://raw.githubusercontent.com/I8552-CM/Ota/delos-pac/updater-old-release.json
 PRODUCT_PROPERTY_OVERRIDES += ro.rom.version=$(shell date +%Y%m%d)
 PRODUCT_PROPERTY_OVERRIDES += ro.otaupdate.enable_toast=true
 PRODUCT_PROPERTY_OVERRIDES += ro.otaupdate.enable_log=true
